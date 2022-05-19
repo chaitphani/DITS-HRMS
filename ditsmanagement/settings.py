bdd6bf9347d4904e7d3611d1d7d365944c865282
chaitanya <44922027+chaitphani@users.noreply.github.com>
2022-05-19 11:42:04 +0530

    Update settings.py file
    Removed mail details from settings.py file

--- ditsmanagement/settings.py
+++ ditsmanagement/settings.py
@@ -40,8 +40,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ditstaskmanager@gmail.com'
-EMAIL_HOST_PASSWORD = 'Dits@123'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 
 INSTALLED_APPS = [
